ed64f2f2a90799c2fbcc9bd2301528bee43c3852
Naveen Kumar <metanaveen@outlook.com>
2017-01-11 16:22:18 +0530

    First commit

---
+++ www/js/app.js
@@ -0,0 +1,73 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'ionic.contrib.ui.tinderCards'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.directive('noScroll', function() {
+    return {
+        restrict: 'A',
+        link: function($scope, $element, $attr) {
+            $element.on('touchmove', function(e) {
+                e.preventDefault();
+            });
+        }
+    }
+})
+
+.controller('CardsCtrl', function($scope) {
+
+    $scope.test = function() {
+        alert("HI");
+    };
+
+    var cardTypes = [
+        { image: 'img/pic2.png', title: 'So much grass #hippster'},
+        { image: 'img/pic3.png', title: 'Way too much Sand, right?'},
+        { image: 'img/pic4.png', title: 'Beautiful sky from wherever'},
+    ];
+ 
+    $scope.cards = [];
+ 
+    $scope.addCard = function(i) {
+        var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+        newCard.id = Math.random();
+        $scope.cards.push(angular.extend({}, newCard));
+    }
+ 
+    // for(var i = 0; i < 3; i++) $scope.addCard();
+    for(var i = 0; i < cardTypes.length; i++) $scope.addCard();
+ 
+    $scope.cardSwipedLeft = function(index) {
+        console.log('Left swipe');
+    }
+ 
+    $scope.cardSwipedRight = function(index) {
+        console.log('Right swipe');
+    }
+ 
+    $scope.cardDestroyed = function(index) {
+        $scope.cards.splice(index, 1);
+        console.log('Card removed');
+    }
+})
+;
